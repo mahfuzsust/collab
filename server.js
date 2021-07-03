7eb3d54e9eb50409a872cef715623b9d3fbbc411
Mahfuzur Rahman <mahfuz.sust001@gmail.com>
2021-07-03 20:56:15 +0600

    add redis adapter

--- server.js
+++ server.js
@@ -6,22 +6,30 @@ const { v4: uuidv4 } = require('uuid');
 const app = express();
 const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer);
+const { createClient } = require('redis');
+const redisAdapter = require('@socket.io/redis-adapter');
+const expirationSeconds = 12 * 60 * 60;
+
+const pubClient = createClient({ host: process.env.REDIS_ENDPOINT || 'localhost', port: 6379 });
+const subClient = pubClient.duplicate();
+io.adapter(redisAdapter(pubClient, subClient));
 
 app.use('/public', express.static('public'));
 
 app.get('/:id', (req, res) => {
     const fileDirectory = path.resolve(__dirname);
     res.sendFile('index.html', { root: fileDirectory }, (err) => {
+        if (err) {
+            console.error(err);
+            throw (err);
+        }
         res.end();
-        if (err) throw (err);
     });
 });
 app.get('/', (req, res) => {
     res.redirect(307, '/' + uuidv4());
 });
 
-let roomMembers = {};
-let socketRoom = {};
 
 io.on("connection", socket => {
     console.log('socket connected..', socket.id);
@@ -35,29 +43,50 @@ io.on("connection", socket => {
         const room = data.documentId;
         socket.join(room);
 
-        socketRoom[socket.id] = room;
-        if (roomMembers[room]) {
-            roomMembers[room].push({ id: socket.id, name: data.handle });
-        } else {
-            roomMembers[room] = [{ id: socket.id, name: data.handle }];
-        }
+        pubClient.set("room_" + socket.id, room);
+        pubClient.expire("room_" + socket.id, expirationSeconds);
+
+        pubClient.get("members_" + room, function (err, membersResponse) {
+            let members;
+            if (membersResponse) {
+                members = JSON.parse(membersResponse)
+                members.push({ id: socket.id, name: data.handle });
+            } else {
+                members = [{ id: socket.id, name: data.handle }];
+            }
+            pubClient.set("members_" + room, JSON.stringify(members));
+            pubClient.expire("members_" + room, expirationSeconds);
+
+            io.in(room).emit('members', members);
+        });
 
         socket.to(room).emit('register', { id: socket.id, name: data.handle });
-        io.in(room).emit('members', roomMembers[room]);
     });
     socket.on('disconnect', function (data) {
         console.log("Disconnected")
-        let room = socketRoom[socket.id];
-        if (room) {
-            roomMembers[room] = roomMembers[room].filter(function (item) {
-                return item.id !== socket.id
-            });
-            if (roomMembers[room].length == 0) {
-                delete roomMembers[room];
+
+        pubClient.get("room_" + socket.id, function (err, room) {
+            if (room) {
+                pubClient.get("members_" + room, function (err2, membersResponse) {
+                    if (membersResponse) {
+                        let members = JSON.parse(membersResponse)
+                        members = members.filter(function (item) {
+                            return item.id !== socket.id
+                        });
+
+                        if (members.length == 0) {
+                            pubClient.del("members_" + room);
+                        } else {
+                            pubClient.set("members_" + room, JSON.stringify(members));
+                            pubClient.expire("members_" + room, expirationSeconds);
+                        }
+                    }
+                });
+                pubClient.del("room_" + socket.id);
+                socket.to(room).emit('user_left', { id: socket.id });
             }
-            delete socketRoom[socket.id];
-            socket.to(room).emit('user_left', { id: socket.id });
-        }
+        });
+
     });
 });
 
